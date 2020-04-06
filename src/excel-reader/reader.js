37e3a5ed5ff4de5ac97bfa2a49f66b3dfac51766
gabriel_ajabahian <gabriel_ajabahian@mckinsey.com>
2020-04-06 19:14:57 +0400

    Fix: Fixing the output of the data

--- src/excel-reader/reader.js
+++ src/excel-reader/reader.js
@@ -5,7 +5,7 @@ module.exports = {
         console.log("Reading from Xlsx file location: " + filePath);
         var workbook = xlsx.readFile(filePath);
         var sheet_name_list = workbook.SheetNames; // SheetNames is an ordered list of the sheets in the workbook
-        var jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]],{defval:""});
+        var jsonData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]],{defval:null, header:1});
         var excelData = new excelDataModel(sheet_name_list[0], jsonData);
         return excelData;
     }
